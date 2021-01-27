fa2058085253e5364b232d04e6a1b1300e6768eb
Aysia Brown <aysiaebrown@gmail.com>
2021-01-26 20:17:00 -0800

    updated storefront to handle adding to cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Categories from './components/categories';
 import Header from './components/header';
 import Products from './components/products';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
+import Cart from './simpleCart'
 
 function Header(props) {
 
@@ -12,6 +13,7 @@ function Header(props) {
         <Typography component="div" style={{ backgroundColor: '#f5e9ec', height: '75px', width: '100vw' }}>
             <Typography variant='h2' component='h2'>SAD GIRL GAMES</Typography>
         </Typography>
+        <Cart/>
         </Container>
         </>
         
